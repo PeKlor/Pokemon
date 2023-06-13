2e206b866e3cd819eba324b1ce38f224512cecfd
PeKlor <peklor7@gmail.com>
2023-06-13 21:24:35 +0300

    Charmander

--- Pokemon.py
+++ Pokemon.py
@@ -1,15 +1,24 @@
 from Type.py import Type
 
+def initialize_Moveset(moveset):
+        moveset = ["", "", "", ""]
+
 class Pokemon():
 
-    def __init__(self, name, type, moveset):
+    def __init__(self, name, type: Type, moveset, cry):
         self.name = name
         self.type = type
         self.moveset = moveset
+        initialize_Moveset(self.moveset)
+        self.cry = cry
 
     def get_Name(self):
         return self.name
     
     def get_Type(self):
         return self.type
+
+    def get_Cry(self):
+        return self.cry
+
 
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 # Welcome to the world of Pokemon!
+import Type.py
+from Pokemon import Pokemon
 
 available_Types = ["Fire", "Water", "Grass"]
 
+charmander_Moveset = ["Tackle", "Ember", "Tail Whip", "Growl"]
+charmander = Pokemon("Charmander", "Fire", charmander_Moveset, "Char Char!")
+
+print(charmander.get_Name() + " attacks! " + charmander.get_Cry())
 
 
